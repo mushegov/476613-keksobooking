f9f6328462d9d65f69feb1dcf34e4484408ef4b9
Vladimir Mushegov <mushegov@mail.ru>
2018-02-26 09:21:13 +0600

    Добавил модуль отвечающий за фильтрацию геометок на карте

---
+++ js/filter.js
@@ -0,0 +1,86 @@
+'use strict';
+
+(function () {
+  //
+  var form = document.querySelector('.map__filters');
+
+  //
+  var filterData = function () {
+    var filteredData = window.backend.data;
+    var formData = new FormData(form);
+    var features = [];
+
+    //
+    form.querySelectorAll('.map__filter-set input:checked').forEach(function (checkbox) {
+      features.push(checkbox.value);
+    });
+
+    //
+    if (formData.get('housing-type') !== 'any') {
+      filteredData = filteredData.filter(function (pin) {
+        return pin.offer.type === formData.get('housing-type');
+      });
+    }
+
+    //
+    if (formData.get('housing-price') !== 'any') {
+      filteredData = filteredData.filter(function (pin) {
+        var price = 'middle';
+
+        if (pin.offer.price < 10000) {
+          price = 'low';
+        } else if (pin.offer.price > 50000) {
+          price = 'high';
+        }
+
+        return price === formData.get('housing-price');
+      });
+    }
+
+    //
+    if (formData.get('housing-rooms') !== 'any') {
+      filteredData = filteredData.filter(function (pin) {
+        return pin.offer.rooms === parseInt(formData.get('housing-rooms'), 10);
+      });
+    }
+
+    //
+    if (formData.get('housing-guests') !== 'any') {
+      filteredData = filteredData.filter(function (pin) {
+        return pin.offer.guests === parseInt(formData.get('housing-guests'), 10);
+      });
+    }
+
+    //
+    if (features) {
+      features.forEach(function (feature) {
+        filteredData = filteredData.filter(function (pin) {
+          return pin.offer.features.includes(feature);
+        });
+      });
+
+    }
+
+    return filteredData;
+  };
+
+  //
+  form.addEventListener('change', function () {
+    var filteredData = filterData();
+
+    window.pins.render(filteredData);
+
+    if (!filteredData.length) {
+      window.page.showError('Нет подходящих объявлений. Измените фильтр.', 2500);
+    }
+
+    console.log(filteredData);
+  });
+
+
+  // EXPORT
+  window.filter = {
+    apply: 'apply',
+    reset: 'reset'
+  };
+})();
