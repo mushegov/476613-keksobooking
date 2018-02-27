b9b542a8a062f10482dd53429fa98398cccb3a21
Vladimir Mushegov <mushegov@mail.ru>
2018-02-27 15:35:10 +0600

    Переписал рендер меток Теперь одна функция рендерит дом элементы, а вторая показывает те или иные геометки

--- js/pins.js
+++ js/pins.js
@@ -8,44 +8,61 @@
   var MAX_PINS = 5;
 
 
-  // Отрисовываем геометки
-  var renderMapPins = function (array) {
-    removeMapPins();
-
+  // Генерируем разметку геометки
+  var renderPins = function (array) {
     var template = window.util.templates.pin;
 
     var fragment = document.createDocumentFragment();
 
-    var pinsAmount = array.length < MAX_PINS ? array.length : MAX_PINS;
+    var pinsArray = [];
 
     // Генерируем элемент для каждого объявления и добавляем его во фрагмент
-    for (var i = 0; i < pinsAmount; i++) {
+    for (var i = 0; i < array.length; i++) {
       var element = template.cloneNode(true);
 
+      element.style.display = 'none';
       element.style.left = array[i].location.x + 'px';
       element.style.top = array[i].location.y - PIN_OFFSET + 'px';
       element.querySelector('img').src = array[i].author.avatar;
       element.setAttribute('data-pin', i);
+      element.setAttribute('id', 'pin' + i);
 
       fragment.appendChild(element);
+
+      array[i].id = i;
+      pinsArray.push(i);
     }
 
     document.querySelector('.map__pins').appendChild(fragment);
+
+    showPins(pinsArray);
   };
 
-  // Убираем геометк
-  var removeMapPins = function () {
+  // Показываем геометки
+  var showPins = function (array) {
+    hidePins();
+
+    array.forEach(function (item, i) {
+      if (i < MAX_PINS) {
+        document.querySelector('#pin' + i).style.display = 'block';
+      }
+    });
+  };
+
+  // Скрываем геометки
+  var hidePins = function () {
     var pins = document.querySelectorAll('.map__pin:not(.map__pin--main)');
 
     pins.forEach(function (pin) {
-      pin.remove();
+      pin.style.display = 'none';
     });
   };
 
 
   // EXPORT
   window.pins = {
-    render: renderMapPins,
-    remove: removeMapPins
+    render: renderPins,
+    show: showPins,
+    hide: hidePins
   };
 })();
